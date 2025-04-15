35b33e001926e8414b87a72520a35c9386256b78
Виолетта Евдокимова <72455185+Ewdokimova20@users.noreply.github.com>
2025-04-15 06:25:17 +0000

    добавили стили отельным файлом,  пока не встроенны в основной файл

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"; // Импортируем компонент Home
 import RegistrationForm from "./components/RegistrationForm"; // Если используете отдельно
+import './styles.css'; // Импортируйте файл стилей
 
 const App = () => {
   return (
